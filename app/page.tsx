c2cca0e5dde2b56ddf267605f396474bad974c5b
joeycannoliLABS <joey.cuccaro@avalabs.org>
2026-05-20 15:03:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,11 +48,10 @@ export default function Home() {
     <>
       {/* Header with Avalanche logo */}
       <div style={{ marginBottom: 36 }}>
+        <div style={{ marginBottom: 16 }}>
+          <img src="/avalanche-horizontal.svg" alt="Avalanche" height={32} style={{ display: "block" }} />
+        </div>
         <div style={{ display: "flex", alignItems: "center", gap: 14, marginBottom: 8 }}>
-          <svg width="36" height="36" viewBox="0 0 255 255" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M171.8 167.3H222.2C229.4 167.3 233 167.3 234.8 165.7C236.9 163.8 237 161.4 235.8 159L185.4 69.4C183.3 65.7 180 59.4 176.8 59.4C173.6 59.4 170.3 65.7 168.2 69.4L156.7 89.9L156.6 90.1C154.2 94.4 152.9 96.7 152.2 99.1C151.4 101.8 151.4 104.7 152.2 107.4C152.9 109.8 154.3 112.2 156.8 116.5L171.6 142.8L171.7 143C174.2 147.3 175.5 149.6 176.1 152C176.9 154.7 176.9 157.6 176.1 160.3C175.5 162.6 174.1 165 171.8 167.3Z" fill="#E84142"/>
-            <path d="M118.3 167.3H33.4C26.2 167.3 22.6 167.3 20.8 165.7C18.7 163.8 18.6 161.4 19.8 159L72.3 65.4C74.4 61.7 77.7 55.4 80.9 55.4C84.1 55.4 87.4 61.7 89.5 65.4L118.5 116.5L118.7 116.9C121.1 121.2 122.4 123.5 123.1 125.9C123.9 128.6 123.9 131.5 123.1 134.2C122.4 136.6 121 139 118.5 143.3L118.3 143.7L118.3 167.3Z" fill="#E84142"/>
-          </svg>
           <h1 style={{ fontSize: 32, fontWeight: 600, margin: 0, color: "var(--text-primary)" }}>
             Prediction Markets
           </h1>
